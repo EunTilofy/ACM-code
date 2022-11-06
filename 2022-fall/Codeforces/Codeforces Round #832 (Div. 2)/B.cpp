c6d971b0edc237089f2e6cf3b24921deccb8b9bf
Duckforces <papercloud@foxmail.com>
2022-11-06 21:38:45 +0800

    Codeforces div2

---
+++ 2022-fall/Codeforces/Codeforces Round #832 (Div. 2)/B.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#define ll long long
+//#define int long long
+#define db double
+#define ld long double
+#define dbg1(x) cerr<<#x<<"="<<(x)<<" "
+#define dbg2(x) cerr<<#x<<"="<<(x)<<"\n"
+#define dbg3(x) cerr<<#x<<"\n"
+using namespace std;
+#define reg register
+#define inf 0x3f3f3f3f
+#define infL 0x3f3f3f3f3f3f3f3f
+typedef pair<int,int> pii;
+#define REP(i,a,b) for(int i=(a),i##_end_=(b);i<i##_end_;++i)
+#define DREP(i,a,b) for(int i=(a),i##_end_=(b);i>i##_end_;--i)
+#define For(i,a,b) for(int i=(a);i<=(b);++i)
+#define FOr(i,a,b) for(int i=(a);i>=(b);--i)
+#define y1 y11111111111
+#define pb push_back
+#define mkp make_pair
+#define fi first
+#define se second
+#define mod 998244353
+inline int qpow(int x,ll y,int m=mod){int r=1;for(;y;y>>=1,x=(ll)x*x%m)if(y&1)r=(ll)r*x%m;return r;}
+inline int Add(int x, int y){return (x + y) % mod;}
+inline int Mul(int x, int y){return 1ll * x * y % mod;}
+inline int read()
+{
+    int x=0,f=1;char ch=getchar();
+    while(ch<'0'||ch>'9'){if(ch=='-')f=-1;ch=getchar();}
+    while(ch>='0'&&ch<='9'){x=(x<<3)+(x<<1)+ch-'0';ch=getchar();}
+    return x*f;
+}
+const int MN = 3e5 + 5;
+int a[MN], b[MN], c[MN], n, m;
+signed main() {
+    // freopen("out.txt", "w", stdout);
+    // freopen("in.txt", "r", stdin);
+    std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
+    int T = 1;
+    cin>>T;
+    while(T--) {
+        cin>>n;
+        int i = 1, j = 3 * n;
+        vector<pii> ans;
+        while(i < j) {
+            ans.push_back({i, j});
+            i += 3;
+            j -= 3;
+        }
+        cout<<ans.size()<<endl;
+        for(auto x : ans) cout<<x.fi<<" "<<x.se<<endl;
+        // cout<<(n + 1) / 2<<endl;
+        // for(int i = 1; i <= n; i += 2) {
+        //     if(i == n) cout<<3*i-2<<" "<<3*i-1<<endl;
+        //     else cout<<3*i-1<<" "<<3*(i+1)<<endl;
+        // }
+    }
+    return 0;
+}
