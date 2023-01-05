e2d70348bc2be6de9f337556367a20b4a527a1e3
itolfy <itolfy@outlook.com>
2023-01-05 22:25:44 +0800

    cf goodbye 2022

---
+++ 2022-fall/Codeforces/Codeforces Round #842 (Div. 2)/mod_new.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define all(x) x.begin(),x.end()
+#define fi first
+#define se second
+#define pb push_back
+#define mkp make_pair
+#define mod 998244353
+inline int qpow(int x,ll y,int m=mod){int r=1;for(;y;y>>=1,x=(ll)x*x%m)if(y&1)r=(ll)r*x%m;return r;}
+inline int Add(int x, int y){return (x + y) % mod;}
+inline int Mul(int x, int y){return 1ll * x * y % mod;}
+const int MN = 3e5 + 5;
+int a[MN], b[MN], c[MN];
+const int inv2 = (mod + 1)/2, inv3 = (mod + 1)/3, inv4 = (mod * 2 + 1) / 4;
+
+void solve(){
+	int n, m;
+	
+}
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);
+	int T;
+	cin>>T;
+	while(T--) solve();
+}
